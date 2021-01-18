cc955c88d8ac90fda800a7e9c25c7951b5074e6f
spolti <fspolti@redhat.com>
2021-01-18 19:53:59 -0300

    Small fixes, remove services file from spi readme

--- rebot-services/rebot-persistence-service/src/main/java/it/rebase/rebot/service/persistence/pojo/Fact.java
+++ rebot-services/rebot-persistence-service/src/main/java/it/rebase/rebot/service/persistence/pojo/Fact.java
@@ -28,8 +28,6 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
