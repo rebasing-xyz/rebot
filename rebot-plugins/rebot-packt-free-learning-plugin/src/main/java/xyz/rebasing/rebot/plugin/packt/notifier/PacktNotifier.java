0f2b3def0a7751bb4c404eb53c192a5e73fe5870
spolti <fspolti@redhat.com>
2022-08-24 09:29:05 -0300

    update packt plugin, v1 api is no longer available

--- rebot-plugins/rebot-packt-free-learning-plugin/src/main/java/xyz/rebasing/rebot/plugin/packt/notifier/PacktNotifier.java
+++ rebot-plugins/rebot-packt-free-learning-plugin/src/main/java/xyz/rebasing/rebot/plugin/packt/notifier/PacktNotifier.java
@@ -23,13 +23,11 @@
 
 package xyz.rebasing.rebot.plugin.packt.notifier;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.lang.invoke.MethodHandles;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -49,7 +47,6 @@ import xyz.rebasing.rebot.api.domain.MessageUpdate;
 import xyz.rebasing.rebot.api.i18n.I18nHelper;
 import xyz.rebasing.rebot.api.shared.components.message.sender.OutcomeMessageProcessor;
 import xyz.rebasing.rebot.plugin.packt.domain.DailyOffer;
-import xyz.rebasing.rebot.plugin.packt.domain.LoadDailyOffer;
 import xyz.rebasing.rebot.service.persistence.domain.PacktNotification;
 import xyz.rebasing.rebot.service.persistence.repository.PacktRepository;
 
@@ -58,8 +55,7 @@ public class PacktNotifier {
 
     private final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
-    private static final String FREE_LEARNING_URL = "https://www.packtpub.com/packt/offers/free-learning";
-    private static final String PACKT_LOAD_OFFER_ENDPOINT = "https://services.packtpub.com/free-learning-v1/offers?dateFrom=%sT00:00:00.000Z&dateTo=%sT00:00:00.000Z";
+    private static final String FREE_LEARNING_URL = "https://www.packtpub.com/free-learning";
     private static final String PACKT_DAILY_OFFER_ENDPOINT = "https://static.packt-cdn.com/products/%s/summary";
 
     Cache<String, Object> cache = Caffeine.newBuilder().build();
@@ -69,6 +65,8 @@ public class PacktNotifier {
 
     @Inject
     private PacktRepository repository;
+    @Inject
+    ObjectMapper objectMapper;
 
     public String get(String locale) {
         DailyOffer dailyOffer = (DailyOffer) cache.getIfPresent("book");
@@ -87,17 +85,14 @@ public class PacktNotifier {
         if (log.isDebugEnabled()) {
             log.debug("Retrieving information about the daily free ebook.");
         }
-        Date today = new Date();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate localDate = today.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS))
                 .build();
 
+        // need to query for the product id now, updated, again.
         Request loadDailyOfferRequest = new Request.Builder()
-                .url(String.format(PACKT_LOAD_OFFER_ENDPOINT, localDate.format(formatter),
-                                   localDate.plus(1, ChronoUnit.DAYS).format(formatter)))
+                .url(FREE_LEARNING_URL)
                 .get()
                 .build();
 
@@ -109,39 +104,42 @@ public class PacktNotifier {
                           response.code());
             }
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            LoadDailyOffer loadDailyOffer = objectMapper.readValue(response.body().string(), LoadDailyOffer.class);
-            if (log.isDebugEnabled()) {
-                log.debug(loadDailyOffer.toString());
+            BufferedReader reader = new BufferedReader(new StringReader(response.body().string()));
+            // navigate directly to the end of the html to get the product ID and get the 'const metaProductId' value
+            // it is at the end of the html page.
+            Optional<String> metaProductId = reader.lines().skip(1616)
+                    .filter(s -> s.contains("const metaProductId"))
+                    .map(s -> s.split("=")[1].replace("\'", "").replace(";", "").trim())
+                    .findFirst();
+
+            if (metaProductId.isPresent()) {
+                Request getDailyOfferRequest = new Request.Builder()
+                        .url(String.format(PACKT_DAILY_OFFER_ENDPOINT, metaProductId.get()))
+                        .get()
+                        .build();
+
+                Response getDailyOfferResponse = client.newCall(getDailyOfferRequest).execute();
+                if (log.isTraceEnabled()) {
+                    log.tracev("Getting daily pack offer with url: {0}", getDailyOfferRequest.url());
+                }
+                if (getDailyOfferResponse.code() != 200) {
+                    log.warnv("Failed to connect in the endpoint {0}, status code is: {1}",
+                              getDailyOfferRequest.url(), getDailyOfferResponse.code());
+                }
+
+                DailyOffer dailyOffer = objectMapper.readValue(getDailyOfferResponse.body().string(), DailyOffer.class);
+                if (log.isDebugEnabled()) {
+                    log.debug(dailyOffer.toString());
+                }
+
+                cache.invalidateAll();
+                cache.put("book", dailyOffer);
+
+                repository.get().stream().forEach(packtNotification ->
+                                                          this.notify(packtNotification.getChatId(),
+                                                                      packtNotification.getLocale())
+                );
             }
-
-            Request getDailyOfferRequest = new Request.Builder()
-                    .url(String.format(String.format(PACKT_DAILY_OFFER_ENDPOINT, loadDailyOffer.getData().get(0).getProductId())))
-                    .get()
-                    .build();
-
-            if (log.isTraceEnabled()) {
-                log.tracev("Getting daily pack offer with URI: {0}", getDailyOfferRequest.url());
-            }
-
-            Response getDailyOfferResponse = client.newCall(getDailyOfferRequest).execute();
-            if (getDailyOfferResponse.code() != 200) {
-                log.warnv("Failed to connect in the endpoint {0}, status code is: {1}",
-                          getDailyOfferRequest.url(), getDailyOfferResponse.code());
-            }
-
-            DailyOffer dailyOffer = objectMapper.readValue(getDailyOfferResponse.body().string(), DailyOffer.class);
-            if (log.isDebugEnabled()) {
-                log.debug(dailyOffer.toString());
-            }
-
-            cache.invalidateAll();
-            cache.put("book", dailyOffer);
-
-            repository.get().stream().forEach(packtNotification ->
-                                                      this.notify(packtNotification.getChatId(),
-                                                                  packtNotification.getLocale())
-            );
         } catch (final Exception e) {
             e.printStackTrace();
             log.warnv("Failed to obtain the ebook information: {0}", e.getMessage());
